9700ec121c17067a07d9b5117a565c5827494620
Yosoi <yosoi@protonmail.com>
2020-03-16 12:03:20 -0400

    basic functionality

--- Assets/Seedling.cs
+++ Assets/Seedling.cs
@@ -5,7 +5,28 @@ namespace Seedling
   [CreateAssetMenu(menuName = "Seedling")]
   public class Seedling : ScriptableObject
   {
-    public float Amplitude;
-    public float Frequency;
+    private const float SCALE = 0.9f;
+
+    public float Amplitude = 1;
+    public float Frequency = 1;
+    public bool Inverted;
+
+    public float Sample(float x, float y) => Sample(new Vector2(x, y));
+
+    public float Sample(float x) => Sample(new Vector2(x, 0));
+
+    public float Sample(Vector2 point)
+    {
+      point *= SCALE;
+
+      float sample = Mathf.PerlinNoise(point.x * Frequency, point.y * Frequency) * Amplitude;
+
+      if (Inverted)
+      {
+        sample = 1f - sample;
+      }
+      
+      return Mathf.Clamp01(sample);
+    }
   }
 }
--- Assets/SeedlingEditor.cs
+++ Assets/SeedlingEditor.cs
@@ -1,14 +1,58 @@
 namespace Seedling
 {
+  using System.Collections.Generic;
   using UnityEditor;
   using UnityEngine;
 
   [CustomEditor(typeof(Seedling))]
   public class SeedlingEditor : Editor
   {
+    private const int PREVIEW_SIZE = 256;
+
+    private Texture2D previewTexture;
+
+    private void Awake()
+    {
+      previewTexture = new Texture2D(PREVIEW_SIZE, PREVIEW_SIZE);
+
+      UpdatePreviewTexture();
+    }
+
     public override void OnInspectorGUI()
     {
+      GUILayout.Label(previewTexture);
+
+      EditorGUI.BeginChangeCheck();
+
       DrawDefaultInspector();
+
+      if (EditorGUI.EndChangeCheck())
+      {
+          UpdatePreviewTexture();
+      }
+    }
+
+    private void UpdatePreviewTexture()
+    {
+      List<Color> pixels = new List<Color>();
+
+      Seedling seedling = (Seedling)target;
+
+      for (int x = 0; x < PREVIEW_SIZE; x++)
+      {
+        for (int y = 0; y < PREVIEW_SIZE; y++)
+        {
+          float pixelValue = seedling.Sample(x, y);
+
+          Color pixelColor = new Color(pixelValue, pixelValue, pixelValue, 1);
+
+          pixels.Add(pixelColor);
+        }
+      }
+
+      previewTexture.SetPixels(pixels.ToArray());
+
+      previewTexture.Apply();
     }
   }
 }
